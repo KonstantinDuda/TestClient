ca61b7b4ce7025e99a7758db5744c6556eeba9e4
KonstantinDuda <52273304+KonstantinDuda@users.noreply.github.com>
2024-03-07 21:34:58 +0200

    Get Questions
    Get Questions now. Not  String or List<dynamic>

--- bloc/provider_bloc.dart
+++ bloc/provider_bloc.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+import 'package:dio/dio.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:http/http.dart' as http;
 
+import '../events_states/questions.dart';
 import '../events_states/provider_es.dart';
 
 class ProviderBloc extends Bloc<ProviderEvent, ProviderState> {
@@ -10,7 +14,32 @@ class ProviderBloc extends Bloc<ProviderEvent, ProviderState> {
     on<TestProviderEvent>((event, emit) => emit(TestProviderState()));
   }
 
-  _loading(LoadingProviderEvent event, Emitter emit) {
+  _loading(LoadingProviderEvent event, Emitter emit) async {
+    var url = Uri.parse('http://localhost:8000/questions/');
+    var response = await http.get(
+      url,
+      //headers: {"Content-Type": "application/json"},
+    );
+    /*toJson());*/ /*{'name': loginText, 'password': passwordText});*/
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+
+    List<Question> mylist = [];
+    //mylist.addAll(jsonDecode(response.body));
+
+    //var mybody = response.body;
+    var mybody = jsonDecode(response.body);
+    //var data = Question.fromMap(mybody as Map<String, dynamic>);
+    //var questions = Question.fromMap(data);
+    //print('list ==  $mylist');
+    for (var element in mybody) {
+      mylist.add(Question.fromJson(element));
+    }
+    print('mybody: $mybody');
+    //print(response.body.runtimeType);
+    print(mybody.runtimeType);
+    print(mylist);
+
     emit(RootTestsProviderState());
   }
 }
